dcd29f121fec6685e5fdddc961057688a512d96c
Allen Suvorov <allen.suvorov@gmail.com>
2023-09-27 20:16:35 -0600

    reading input.txt

--- training1/home-work-for-lecture1/a-ac/main.go
+++ training1/home-work-for-lecture1/a-ac/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	content, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(content))
+}
+
+func resTemp(data []byte) int {
+	return 0
+}
