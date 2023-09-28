c1d51fa1ccea5e7bb461aa952e3dc06a2867b9fb
Allen Suvorov <allen.suvorov@gmail.com>
2023-09-27 20:38:00 -0600

    feat: getArgs
    * parse input data
    * split into args

--- training1/home-work-for-lecture1/a-ac/main.go
+++ training1/home-work-for-lecture1/a-ac/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -14,6 +16,24 @@ func main() {
 	fmt.Println(string(content))
 }
 
-func resTemp(data []byte) int {
+func getArgs(data []byte) (int, int, string) {
+	lines := strings.Split(string(data), "\n")
+	tRoom, err := strconv.Atoi(strings.Fields(lines[0])[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	tCond, err := strconv.Atoi(strings.Fields(lines[0])[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	mode := lines[1]
+	return tRoom, tCond, mode
+}
+
+func resTemp(tRoom, tCond int, mode string) int {
+	"freeze" // can only decrease temp, if needed
+	"heat"   // can only increase temp, if needed
+	"auto"   // does both
+	"fan"    // doesn't change temp
 	return 0
 }
